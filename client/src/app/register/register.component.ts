9c3868dd2e9610ca53ab1b66d642bc6c974bcc64
VIA\uryga <uryga@vialutions.com>
2021-07-06 14:38:35 +0200

    initial commit

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() usersFromHomeComponent: User[];
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    }, error => {
+      this.toastr.error(error.error)
+    })
+  }
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
